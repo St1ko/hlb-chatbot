e3f892e58a1d3d7e609026f6ba49df90ad15cd59
NonPon <33087527+NonPon@users.noreply.github.com>
2020-04-02 17:49:42 +0200

    Basis chatbot (#2)
    * Verandering Library file
    
    Comments toegevoegd wat de .js files zijn en mijn eigen aangepaste library geadd zodat je afbeeldingen kan laten staan in berichten van de gebruiker (kan weer teruggezet worden naar online versie als we geen afbeeldingen gebruiken)
    
    * comments juist erin gezet
    
    stonden er eerst verkeerd in
    
    * Muli font
    
    Added muli fontfamily to scss typography file
    
    * Styling
    
    Begonnen aan styling van de chatbot. 
    -tekstwolken zijn af
    -textinput velden zijn af 
    -begin aan styling van het chatbot venster
    -!!!rating buttons styling nog niet werkend!!!
    
    Co-authored-by: Tom-X-S <txsomhorst@gmail.com>

--- js/bot.js
+++ js/bot.js
@@ -2,16 +2,135 @@
 var botui = new BotUI("hello-world");
 
 //#5 API call
-botui.message
-  .add({
-    content: "Hello World from bot!"
+botui.message.add({
+  content: "Hello World from bot!"
+})
+.then(function() {
+  botui.message.add({
+    delay: 1000,
+    human: true,
+    content: "Hello World from human!"
   })
   .then(function() {
-    // wait till previous message has been shown.
-
-    botui.message.add({
+    botui.action.button({
       delay: 1000,
-      human: true,
-      content: "Hello World from human!"
+      action: [
+        { // show only one button
+          text: 'One',
+          value: 'one'
+        },
+        { // show only one button
+          text: 'Two',
+          value: 'two'
+        }
+      ]
+    })
+    .then(function (res) { // will be called when a button is clicked.
+      console.log(res.value); // will print "one" from 'value'
+    })
+    .then(function() {
+      botui.message.add({
+        delay: 1000,
+        human: false,
+        content: "Hello World from human!"
+      })
+      .then(function() {
+        botui.action.text({
+          delay: 1000,
+          action: {
+            placeholder: 'Enter your text here'
+          }
+        }).then(function (res) { // will be called when it is submitted.
+          console.log(res.value); // will print whatever was typed in the field.
+        })
+        .then(function() {
+          botui.message.add({
+            delay: 1000,
+            human: false,
+            content: "Hello World from human!"
+          })
+          .then(function() {
+            botui.action.button({
+              delay: 1000,
+              action: [
+                { // show only one button
+                  cssClass: 'botui-actions-ratings-button',
+                  text: '1',
+                  value: '1'
+                },
+                { // show only one button
+                  cssClass: 'botui-actions-ratings-button',
+                  text: '2',
+                  value: '2'
+                },
+                { // show only one button
+                  cssClass: 'botui-actions-ratings-button',
+                  text: '3',
+                  value: '3'
+                },
+                { // show only one button
+                  cssClass: 'botui-actions-ratings-button',
+                  text: '4',
+                  value: '4'
+                },
+                { // show only one button
+                  cssClass: 'botui-actions-ratings-button',
+                  text: '5',
+                  value: '5'
+                }
+              ]
+            })
+            .then(function (res) { // will be called when a button is clicked.
+              console.log(res.value); // will print "one" from 'value'
+            });
+          });
+        });
+      });
     });
   });
+});
+
+
+
+
+
+
+/* custom ratings buttons
+
+.then(function() {
+            botui.action.button({
+              delay: 1000,
+              action: [
+                { // show only one button
+                  cssClass: 'botui-actions-ratings-button',
+                  text: '1',
+                  value: '1'
+                },
+                { // show only one button
+                  cssClass: 'botui-actions-ratings-button',
+                  text: '2',
+                  value: '2'
+                },
+                { // show only one button
+                  cssClass: 'botui-actions-ratings-button',
+                  text: '3',
+                  value: '3'
+                },
+                { // show only one button
+                  cssClass: 'botui-actions-ratings-button',
+                  text: '4',
+                  value: '4'
+                },
+                { // show only one button
+                  cssClass: 'botui-actions-ratings-button',
+                  text: '5',
+                  value: '5'
+                }
+              ]
+            })
+            .then(function (res) { // will be called when a button is clicked.
+              console.log(res.value); // will print "one" from 'value'
+            });
+          });
+
+*/
