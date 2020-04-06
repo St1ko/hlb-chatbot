a243d61930e627432153b0148cba3869c5dbf075
Tom-X-S <txsomhorst@gmail.com>
2020-04-06 14:48:47 +0200

    Volledige styling V1
    -Styling van rating knoppen afgemaakt. 
    -Progress element toegevoegd en gestyled. Momenteel alleen nog een statisch element.
    -Code en styling voor het weergeven van timestamps toegevoegd.

--- js/bot.js
+++ js/bot.js
@@ -5,12 +5,14 @@ var botui = new BotUI("hello-world");
 botui.message.add({
   content: "Hello World from bot!"
 })
+
 .then(function() {
   botui.message.add({
     delay: 1000,
     human: true,
     content: "Hello World from human!"
   })
+
   .then(function() {
     botui.action.button({
       delay: 1000,
@@ -25,15 +27,17 @@ botui.message.add({
         }
       ]
     })
-    .then(function (res) { // will be called when a button is clicked.
+    .then(function (res, index) { // will be called when a button is clicked.
       console.log(res.value); // will print "one" from 'value'
     })
+
     .then(function() {
       botui.message.add({
         delay: 1000,
         human: false,
         content: "Hello World from human!"
       })
+
       .then(function() {
         botui.action.text({
           delay: 1000,
@@ -43,38 +47,36 @@ botui.message.add({
         }).then(function (res) { // will be called when it is submitted.
           console.log(res.value); // will print whatever was typed in the field.
         })
+
         .then(function() {
           botui.message.add({
             delay: 1000,
             human: false,
             content: "Hello World from human!"
           })
+          
           .then(function() {
             botui.action.button({
+              cssClass: 'botui-actions-ratings',
               delay: 1000,
               action: [
                 { // show only one button
-                  cssClass: 'botui-actions-ratings-button',
                   text: '1',
                   value: '1'
                 },
                 { // show only one button
-                  cssClass: 'botui-actions-ratings-button',
                   text: '2',
                   value: '2'
                 },
                 { // show only one button
-                  cssClass: 'botui-actions-ratings-button',
                   text: '3',
                   value: '3'
                 },
                 { // show only one button
-                  cssClass: 'botui-actions-ratings-button',
                   text: '4',
                   value: '4'
                 },
                 { // show only one button
-                  cssClass: 'botui-actions-ratings-button',
                   text: '5',
                   value: '5'
                 }
@@ -90,47 +92,3 @@ botui.message.add({
   });
 });
 
-
-
-
-
-
-/* custom ratings buttons
-
-.then(function() {
-            botui.action.button({
-              delay: 1000,
-              action: [
-                { // show only one button
-                  cssClass: 'botui-actions-ratings-button',
-                  text: '1',
-                  value: '1'
-                },
-                { // show only one button
-                  cssClass: 'botui-actions-ratings-button',
-                  text: '2',
-                  value: '2'
-                },
-                { // show only one button
-                  cssClass: 'botui-actions-ratings-button',
-                  text: '3',
-                  value: '3'
-                },
-                { // show only one button
-                  cssClass: 'botui-actions-ratings-button',
-                  text: '4',
-                  value: '4'
-                },
-                { // show only one button
-                  cssClass: 'botui-actions-ratings-button',
-                  text: '5',
-                  value: '5'
-                }
-              ]
-            })
-            .then(function (res) { // will be called when a button is clicked.
-              console.log(res.value); // will print "one" from 'value'
-            });
-          });
-
-*/
