ec619f813fb7beaa7d8f69bc356c0e7cf33e2f9d
Eva <eva_3staart@hotmail.com>
2020-04-08 12:16:54 +0200

    commit

--- js/bot.js
+++ js/bot.js
@@ -37,11 +37,13 @@ const testQuestions = [
   }
 ];
 
+let CCLink = "https://www.hlb.nl/van-daal/pagina/challenge-control";
+
 let results = [
-  { thema: "Informatie", score: 0 },
-  { thema: "Strategie", score: 0 },
-  { thema: "Mensen", score: 0 },
-  { thema: "Persoonlijk", score: 0 }
+  { thema: "Informatie", score: 0, indepth: "Informatie uitleg"},
+  { thema: "Strategie", score: 0, indepth: "Strategie uitleg"},
+  { thema: "Mensen", score: 0, indepth: "Mensen uitleg"},
+  { thema: "Persoonlijk", score: 0, indepth: "Persoonlijk uitleg"}
 ];
 
 const getLowestScore = function() {
@@ -50,18 +52,20 @@ const getLowestScore = function() {
   );
 };
 
+
+
 const botui = new BotUI("hello-world");
 
 botui.message
   .add({
-    content: "Hallo, ik ben Dirk de Chatbot van HLB Van Daal!"
+    content: "Hallo, ik ben Dirk de virtuele assistent van HLB Van Daal! Wat leuk om je te zien."
   })
   .then(function() {
     botui.message
       .add({
         delay: 500,
         content:
-          "Heeft u hulp nodig bij uw bedrijf? Zo ja, vul de vragenlijst in om contact op te nemen met HLB Van Daal en hun te in te briefen over uw probleem."
+          "Wij hebben binnen HLB Van Daal een Challenge & Control tool ontwikkeld. Deze tool focust op het optimaliseren van uw bedrijf. Heeft u interesse om te zien hoe wij uw bedrijf kunnen helpen te optimaliseren door middel van het afnemen van deze tool?"
       })
       .then(function() {
         botui.action
@@ -89,10 +93,50 @@ const test = function() {
     .add({
       delay: 500,
       content:
-        "Welke van deze onderwerpen zorgen binnen uw bedrijf voor problemen?"
+        "Ik ga je nu een paar vragen stellen om erachter te komen waar wij je mee kunnen helpen. Er komen in totaal 8 uitspraken.Je kan antwoord geven door op de knoppen te drukken van 1 tot 5, waarbij je met 5 eens bent met de uitspraak en 1 er niet eens mee bent. Succes!"
     })
     .then(function() {
-      rateQuestion(0);
+      botui.action
+          .button({
+            human: true,
+            action: [
+              {
+                text: "Doe de test",
+                value: () => rateQuestion(0)
+              },
+              {
+                text: "Meer uitleg",
+                value: () => moreInfo()
+              }
+            ]
+          })
+          .then(function(res) {
+            res.value();
+          });
+    });
+};
+
+const moreInfo = function() {
+  botui.message
+    .add({
+      delay: 500,
+      content:
+        "Tijdens deze test wordt er gefocust op het testen van 4 thema's binnen uw bedrijf: Persoonlijk, Mensen, Strategie en Informatie. Op basis van het resultaat krijgt u een korte uitleg hoe wij u daarmee kunnen helpen. Heeft u nog meer vragen? Dan kunt u <a href="+ CCLink +">hier</a> klikken om te worden doorgeleid naar de website. U kunt ook contact opnemen met een van onze medewerkers via het telefoonnummer: +315863722"
+    })
+    .then(function() {
+      botui.action
+          .button({
+            human: true,
+            action: [
+              {
+                text: "Doe de test",
+                value: () => rateQuestion(0)
+              }
+            ]
+          })
+          .then(function(res) {
+            res.value();
+          });
     });
 };
 
@@ -156,7 +200,7 @@ const uitslag = function() {
       delay: 1000,
       content:
         "Bedankt. We zien dat je nog kan verbeteren op " +
-        getLowestScore().thema
+        getLowestScore().thema + " " + getLowestScore().indepth
     })
     .then(function() {
       contact();
@@ -167,7 +211,7 @@ const contact = function() {
   botui.message
     .add({
       delay: 1000,
-      content: "Hoe kan HLB Van Daal contact met u opnemen?"
+      content: "Om een beter idee te krijgen van uw resultaat en hoe wij u daarmee kunnen helpen, kunt u hieronder uw mailadres of telefoonnummer achterlaten. Een medewerker zal dan zo snel mogelijk contact met u opnemen.<br>Als u liever direct contact opneemt met een van onze medewerkers, kunt u ons bereiken via ons telefoonnummer: <a href="+">+318583758</a>"
     })
     .then(function() {
       botui.action
@@ -181,6 +225,10 @@ const contact = function() {
             {
               text: "Via telefoon",
               value: () => telefoon()
+            },
+            {
+              text: "Zelf contact opnemen",
+              value: () => zelfContact()
             }
           ]
         })
@@ -195,7 +243,7 @@ const mail = function() {
     .text({
       action: {
         sub_type: "email",
-        placeholder: "Enter your text here"
+        placeholder: "Vul hier uw mailadress in"
       }
     })
     .then(function() {
@@ -220,5 +268,12 @@ const eind = function() {
   botui.message.add({
     delay: 500,
     content: "HLB Van Daal zal zo snel mogelijk contact met u opnemen"
+  });
+};
+
+const zelfContact = function() {
+  botui.message.add({
+    delay: 500,
+    content: "U kunt contact opnemen met een van onze medewerkers via het telefoonnummer: +31583728 of ons mailadres: HLB@Vandaal.nl"
   });
 };
