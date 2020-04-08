d38e406156bb19291af488dec883411f9f81d139
St1ko <stijnalbert@hotmail.com>
2020-04-08 18:07:34 +0200

    Create scripts to run application

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + "/client"));
+app.use(express.static(__dirname + '/client'));
 
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "client", "index.html"))
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'client', 'index.html'))
 );
 
 app.listen(8000, () => {
-  console.log("Example app listening on port 8000");
+  console.log('Example app listening on port 8000');
 });
