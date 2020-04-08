bb84ff423eb6d4f4935f92d5953df5ce92b6c5fd
St1ko <stijnalbert@hotmail.com>
2020-04-08 16:27:02 +0200

    prettify button.js

--- client/js/button.js
+++ client/js/button.js
@@ -1,13 +1,11 @@
 const button = document.getElementById('button');
 
 button.onclick = function() {
-    const div = document.getElementById('hlbot');
+  const div = document.getElementById('hlbot');
 
-    if (div.classList.contains('minimized')) {
-        div.classList.remove('minimized');
-    }
-    else {
-        div.classList.add('minimized');
-    }
-
+  if (div.classList.contains('minimized')) {
+    div.classList.remove('minimized');
+  } else {
+    div.classList.add('minimized');
+  }
 };
