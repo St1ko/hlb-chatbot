eb27aeaeeb1a10a34c83ac15a4fa41b7f2a13bba
St1ko <stijnalbert@hotmail.com>
2020-04-08 16:25:33 +0200

    - change botname - remove actions div

--- client/js/button.js
+++ client/js/button.js
@@ -1,7 +1,7 @@
 const button = document.getElementById('button');
 
 button.onclick = function() {
-    const div = document.getElementById('hello-world');
+    const div = document.getElementById('hlbot');
 
     if (div.classList.contains('minimized')) {
         div.classList.remove('minimized');
