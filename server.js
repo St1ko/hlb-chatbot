44b067867ee3a1eb4768a751f2b00e5de1a73bab
St1ko <stijnalbert@hotmail.com>
2020-04-09 10:14:58 +0200

    test different port

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, 'client', 'index.html'))
 );
 
-app.listen(8000, () => {
+app.listen(80, () => {
   console.log('Example app listening on port 8000');
 });
