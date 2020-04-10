004b835fa68130e0310a4f5d067ee0f399aadd67
St1ko <stijnalbert@hotmail.com>
2020-04-10 13:47:38 +0200

    Add LetsEncrypt SSL certificate

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const http = require('http');
 const https = require('https');
 const fs = require('fs');
 const path = require('path');
 const sassMiddleware = require('node-sass-middleware');
 
 const app = express();
-const port = process.env.PORT || 8000;
 const production = process.env.PROD || false;
+const port = production ? 80 : 8000;
 
 app.use(
   sassMiddleware({
@@ -24,20 +25,30 @@ app.get('/', (req, res) =>
 );
 
 if (production) {
-  const key = fs.readFileSync(
-    path.join(__dirname, 'sslcert', 'selfsigned.key')
+  const privateKey = fs.readFileSync(
+    '/etc/letsencrypt/live/chatbot.stijnalbert.nl/privkey.pem',
+    'utf8'
   );
-  const cert = fs.readFileSync(
-    path.join(__dirname, 'sslcert', 'selfsigned.crt')
+  const certificate = fs.readFileSync(
+    '/etc/letsencrypt/live/chatbot.stijnalbert.nl/cert.pem',
+    'utf8'
   );
-  const credentials = { key: key, cert: cert };
+  const ca = fs.readFileSync(
+    '/etc/letsencrypt/live/chatbot.stijnalbert.nl/chain.pem',
+    'utf8'
+  );
 
-  const server = https.createServer(credentials, app);
-  server.listen(port, () => {
-    console.log('HTTPS app listening on port ' + port);
-  });
-} else {
-  app.listen(port, () => {
-    console.log('HTTP app listening on port ' + port);
+  const credentials = { key: privateKey, cert: certificate, ca: ca };
+
+  const httpsServer = https.createServer(credentials, app);
+
+  httpsServer.listen(443, () => {
+    console.log('HTTPS app listening on port 433');
   });
 }
+
+const httpServer = http.createServer(app);
+
+httpServer.listen(port, () => {
+  console.log('HTTP app listening on port ' + port);
+});
