57f6a4963b263bdb595b863b63ed2c0987809e59
St1ko <stijnalbert@hotmail.com>
2020-04-11 12:50:58 +0200

    Use config to configure server

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const path = require('path');
 const sassMiddleware = require('node-sass-middleware');
 
 const app = express();
-const production = process.env.PROD || false;
-const port = production ? 80 : 8000;
+const config = require('./config.json');
+const environment = process.env.NODE_ENV || 'development';
+const environmentConfig = config[environment];
 
 app.use(
   sassMiddleware({
@@ -24,31 +25,42 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, 'client', 'index.html'))
 );
 
-if (production) {
-  const privateKey = fs.readFileSync(
-    '/etc/letsencrypt/live/chatbot.stijnalbert.nl/privkey.pem',
-    'utf8'
-  );
-  const certificate = fs.readFileSync(
-    '/etc/letsencrypt/live/chatbot.stijnalbert.nl/cert.pem',
-    'utf8'
-  );
-  const ca = fs.readFileSync(
-    '/etc/letsencrypt/live/chatbot.stijnalbert.nl/chain.pem',
-    'utf8'
-  );
+const httpsServer = config => {
+  const httpApp = express();
+  const credentials = getCredentials(config.ssl_credentials_path);
 
-  const credentials = { key: privateKey, cert: certificate, ca: ca };
-
-  const httpsServer = https.createServer(credentials, app);
-
-  httpsServer.listen(443, () => {
-    console.log('HTTPS app listening on port 433');
+  httpApp.get('/', function(req, res) {
+    res.redirect('https://' + req.headers.host + req.url);
   });
+
+  http.createServer(httpApp).listen(config.http_port, () => {
+    console.log(`HTTP app listening on port ${config.http_port}`);
+  });
+
+  https.createServer(credentials, app).listen(config.https_port, () => {
+    console.log(`HTTPS app listening on port ${config.https_port}`);
+  });
+};
+
+const httpServer = config => {
+  http.createServer(app).listen(config.http_port, () => {
+    console.log(`HTTP app listening on port ${config.http_port}`);
+  });
+};
+
+const getCredentials = path => {
+  return {
+    key: fs.readFileSync(path + 'privkey.pem', 'utf8'),
+    cert: fs.readFileSync(path + 'cert.pem', 'utf8'),
+    ca: fs.readFileSync(path + 'chain.pem', 'utf8')
+  };
+};
+
+if (
+  environmentConfig.config_id == 'production' &&
+  environmentConfig.ssl_credentials_path
+) {
+  httpsServer(environmentConfig);
+} else {
+  httpServer(environmentConfig);
 }
-
-const httpServer = http.createServer(app);
-
-httpServer.listen(port, () => {
-  console.log('HTTP app listening on port ' + port);
-});
