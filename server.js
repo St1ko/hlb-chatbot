bdd020bff96e4fdfd2785836ee5e12fa7a7f4bcc
Stijn Albert <stijnalbert@hotmail.com>
2020-04-14 10:07:13 +0200

    Enable https (#23)
    * Add https to server
    
    * Use config to configure server

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const path = require('path');
 const sassMiddleware = require('node-sass-middleware');
 
 const app = express();
-const port = process.env.PORT || 8000;
+const config = require('./config.json');
+const environment = process.env.NODE_ENV || 'development';
+const environmentConfig = config[environment];
 
 app.use(
   sassMiddleware({
@@ -20,6 +25,42 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, 'client', 'index.html'))
 );
 
-app.listen(port, () => {
-  console.log('Example app listening on port ' + port);
-});
+const httpsServer = config => {
+  const httpApp = express();
+  const credentials = getCredentials(config.ssl_credentials_path);
+
+  httpApp.get('/', function(req, res) {
+    res.redirect('https://' + req.headers.host + req.url);
+  });
+
+  http.createServer(httpApp).listen(config.http_port, () => {
+    console.log(`HTTP app listening on port ${config.http_port}`);
+  });
+
+  https.createServer(credentials, app).listen(config.https_port, () => {
+    console.log(`HTTPS app listening on port ${config.https_port}`);
+  });
+};
+
+const httpServer = config => {
+  http.createServer(app).listen(config.http_port, () => {
+    console.log(`HTTP app listening on port ${config.http_port}`);
+  });
+};
+
+const getCredentials = path => {
+  return {
+    key: fs.readFileSync(path + 'privkey.pem', 'utf8'),
+    cert: fs.readFileSync(path + 'cert.pem', 'utf8'),
+    ca: fs.readFileSync(path + 'chain.pem', 'utf8')
+  };
+};
+
+if (
+  environmentConfig.config_id == 'production' &&
+  environmentConfig.ssl_credentials_path
+) {
+  httpsServer(environmentConfig);
+} else {
+  httpServer(environmentConfig);
+}
