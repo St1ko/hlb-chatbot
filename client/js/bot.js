e67e2001f6030d9a2d69fe1a122b544dd739c19c
St1ko <stijnalbert@hotmail.com>
2020-04-15 10:47:15 +0200

    Use same quotes

--- client/js/bot.js
+++ client/js/bot.js
@@ -115,14 +115,12 @@ botui.message
       });
   });
 
-
-
 const test = function() {
   botui.message
     .add({
       delay: 1200,
       content:
-        'Ik ga u nu een paar vragen stellen om erachter te komen waar wij u mee kunnen helpen. Er komen in totaal 8 uitspraken over de 4 volgende thema’s: Persoonlijk, Strategie, Informatie en Mensen. U kunt antwoord geven door op de knoppen te drukken van 1 tot 5, waarbij u met 5 eens bent met de uitspraak en 1 niet eens mee bent. Wilt u meer uitleg? Klik dan op “Meer uitleg". Succes!'
+        'Ik ga u nu een paar vragen stellen om erachter te komen waar wij u mee kunnen helpen. Er komen in totaal 8 uitspraken over de 4 volgende thema’s: Persoonlijk, Strategie, Informatie en Mensen. U kunt antwoord geven door op de knoppen te drukken van 1 tot 5, waarbij u met 5 eens bent met de uitspraak en 1 niet eens mee bent. Wilt u meer uitleg? Klik dan op "Meer uitleg". Succes!'
     })
     .then(function() {
       botui.action
@@ -274,7 +272,6 @@ const contact = function() {
     });
 };
 
-
 const immediateContact = function() {
   botui.message
     .add({
@@ -306,7 +303,6 @@ const immediateContact = function() {
         });
     });
 };
-
 
 const mail = function() {
   botui.action
